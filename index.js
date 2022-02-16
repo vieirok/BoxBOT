25d1ac15b60743b45f9f604bbdf79edf3ec48e77
vieirok <99845412+vieirok@users.noreply.github.com>
2022-02-16 23:48:28 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+const Discord = require("discord.js");
+const client = new Discord.Client();
+require("dotenv").config();
+
+client.on("ready", () => {
+    console.log("I'm ready !");
+});
+
+client.on("message", msg => {
+    if (msg.content === "ping") {
+        msg.reply("pong");
+    }
+});
+
+client.login(process.env.BOT_TOKEN);
